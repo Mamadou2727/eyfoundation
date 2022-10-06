4e374a54ebdf72db04ffe016e9ae3e31218d6cf2
Mamadou 2727 <mamadouk393@gmail.com>
2022-10-06 17:55:28 +0200

    uploading codes

---
+++ js/cause_list.js
@@ -0,0 +1,30 @@
+const cause_list = [
+    {
+        image: "images/img-7.png",
+        title: "mmm",
+        description: "eklncdofjpad,ml,",
+        raised: "30",
+        goal:"400",
+        day:"",
+        month:"Feb",
+    },
+    {
+        image: "images/img-7.png",
+        title: "mmm",
+        description: "eklncdofjpad,ml,",
+        raised: "30",
+        goal:"400",
+        day:"",
+        month:"Feb",
+    },
+    {
+        image: "images/img-7.png",
+        title: "mmm",
+        description: "eklncdofjpad,ml,",
+        raised: "30",
+        goal:"400",
+        day:"",
+        month:"Feb",
+    }
+];
+export default cause_list;
